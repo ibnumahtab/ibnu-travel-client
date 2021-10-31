e9cf922506b7da55706e701b02c9f30c8c67b8a1
ibnumahtab <ibnumahtab@gmail.com>
2021-10-31 13:51:28 +0600

    Tour Section Added

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../../Banner/Banner';
+import TourPackages from '../../TourPackages/TourPackages';
 
 const Home = () => {
     return (
         <div>
             <Banner />
+            <TourPackages />
         </div>
     );
 };
